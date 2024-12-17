678b64b9635ea76457a9210a55e02a6f31f63012
syed-zunair-gillani <dev.zunaair@gmail.com>
2024-12-17 16:00:35 +0500

    type

--- app/blog/components/CommentSection.tsx
+++ app/blog/components/CommentSection.tsx
@@ -64,7 +64,7 @@ export default function CommentSection() {
             className="w-full rounded-lg bg-gray-800/50 border border-gray-700 text-gray-300 px-4 py-2 focus:ring-blue-500 focus:border-blue-500"
           />
         </div>
-        <Button type="submit">Post Comment</Button>
+        <Button>Post Comment</Button>
       </form>
 
       {/* Comments List */}
